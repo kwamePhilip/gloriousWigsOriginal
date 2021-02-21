ee89c4fab954016f7bc63b627cdfa769c22b3705
kwamePhilip <you@example.com>
2021-02-21 15:04:06 -0500

    this is just the beginning set up and added the homepage folder and file

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div>
+            <h1>testing the beginnig</h1>
+
+        </div>
+
+    </div>
+)
+
+export default HomePage;
